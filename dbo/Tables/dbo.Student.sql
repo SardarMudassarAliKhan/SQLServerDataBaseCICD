45768cdadc272e93d162e65977393a43e0269816
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-08-01 21:09:48 +0500

    DevOps: CICD For New Database Schema

---
+++ dbo/Tables/dbo.Student.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [StudentID]      INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]      NVARCHAR (50)  NOT NULL,
+    [LastName]       NVARCHAR (50)  NOT NULL,
+    [DateOfBirth]    DATE           NULL,
+    [Gender]         CHAR (1)       NULL,
+    [Email]          NVARCHAR (100) NULL,
+    [PhoneNumber]    NVARCHAR (15)  NULL,
+    [EnrollmentDate] DATE           NOT NULL,
+    [Address]        NVARCHAR (250) NULL,
+    [City]           NVARCHAR (100) NULL,
+    [State]          NVARCHAR (100) NULL,
+    [PostalCode]     NVARCHAR (10)  NULL,
+    [Country]        NVARCHAR (100) NULL,
+    [IsActive]       BIT            DEFAULT ((1)) NULL,
+    PRIMARY KEY CLUSTERED ([StudentID] ASC),
+    UNIQUE NONCLUSTERED ([Email] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IDX_Student_LastName]
+    ON [dbo].[Student]([LastName] ASC);
+
